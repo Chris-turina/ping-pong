3ad31cef662a271c03c80678d37c1622021dc067
chris turina <chris.turina@gmail.com>
2017-09-22 10:25:13 -0700

    console log ping at every number divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,25 @@ var playPingPong = function(input) {
   var output = "";
     for (var i = 0; i <= input; i++) {
       listNumbers.push(i);
+      var number = listNumbers[i]
+      console.log(number);
+      if (number % 15 == 0){
+        console.log("ping-pong");
+      } if (number % 3 == 0) {
+        console.log("ping");
+      } if (number % 5 == 0) {
+        console.log("pong");
+      }
 
 
 
-  }
+
+
+
+    };
   // alert(listNumbers);
-  return(listNumbers);
-}
+return output;
+};
 
 
 
