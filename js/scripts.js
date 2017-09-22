4fac46b2df43b5f800e33273a39ccb13f1fffafc
chris turina <chris.turina@gmail.com>
2017-09-22 11:11:31 -0700

    convert divisibles of 3 to ping, 5 to pong, 15 to pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,14 @@ var playPingPong = function(input) {
     for (var i = 0; i <= input; i++) {
       listNumbers.push(i);
       var number = listNumbers[i]
-      console.log(number);
-      if (number % 15 == 0){
-        console.log("ping-pong");
-      } if (number % 3 == 0) {
-        console.log("ping");
-      } if (number % 5 == 0) {
-        console.log("pong");
+      if (number % 3 == 0 && number % 15 != 0 && number != 0) {
+        number = "ping"
+      } if (number % 5 == 0 && number % 15 != 0 && number != 0) {
+        number = "pong"
+      } if (number % 15 == 0 && number != 0) {
+        number = "pingpong"
       }
+      output = output + number
 
 
 
@@ -20,8 +20,7 @@ var playPingPong = function(input) {
 
 
     };
-  // alert(listNumbers);
-return output;
+  return output;
 };
 
 
